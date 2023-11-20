54116f49c2514c96f01ec03147525c65027f14a5
songbinh <151479515+songbinh@users.noreply.github.com>
2023-11-20 09:46:24 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,38 @@
+import { Text, SafeAreaView, StyleSheet } from 'react-native';
+
+// You can import supported modules from npm
+import { Card } from 'react-native-paper';
+
+// or any files within the Snack
+import AssetExample from './components/AssetExample';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.paragraph}>
+        FULL NAME IS VO LAM SONG BINH,
+        PHONE: 0942132742, 
+        STUDENT: ID 220501001,
+        EMAIL: vhvhvhvh1996@gmail.com,
+        </Text>
+      <Card>
+        <AssetExample />
+      </Card>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'lightgreen',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'left',
+  },
+});
